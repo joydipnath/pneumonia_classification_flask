fb27cc3956df635134fd48ad76a864a5fc19a606
Joydip N <joydipnath@users.noreply.github.com>
2020-10-04 13:02:30 +0530

    Modified the logic to a voting

--- controllers/predict.py
+++ controllers/predict.py
@@ -27,10 +27,12 @@ class Predict:
 
         # model = load_model(project_path + "densenet_model_binary_sigmoid_focalloss.h5", custom_objects={'focal_loss_fixed': focal_loss()})
         # if you wish to just perform inference with your model and not further optimization or training your model,
-        densenet_bin_sigmoid_model = load_model(app.config['MODEL_PATH'] + "densenet_model_binary_sigmoid_focalloss.h5",
-                                                compile=False)
-        # densenet_bin_softmax_model = load_model(app.config['MODEL_PATH'] + "densenet_model_binary.h5", compile=False)
-        # model_inception = load_model(app.config['MODEL_PATH'] + 'model_inception.h5')
+        # densenet_bin_sigmoid_model = load_model(app.config['MODEL_PATH'] + "densenet_model_binary_sigmoid_focalloss.h5",compile=False)
+
+        Final_DenseNet121_model = load_model(app.config['MODEL_PATH'] + "Final_DenseNet121_model.h5", compile=False)
+        Final_DenseNet121_model.load_weights(app.config['MODEL_PATH'] + "Final_DenseNet121_weights.h5")
+        Final_InceptionV3_model = load_model(app.config['MODEL_PATH'] + "Final_InceptionV3_model.h5", compile=False)
+        Final_InceptionV3_model.load_weights(app.config['MODEL_PATH'] + "Final_InceptionV3_weights.h5")
 
         dcm_file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         dcm_data = pydicom.read_file(dcm_file)
@@ -48,26 +50,49 @@ class Predict:
         x = np.expand_dims(x, axis=0)
         image_data = preprocess_input(x)
 
-        prediction = []
+        # Model 1 - DenseNet121
+        pred1 = Final_DenseNet121_model.predict(image_data)
+        # Model 2 - InceptionV3
+        pred2 = Final_InceptionV3_model.predict(image_data)
 
-        prediction.append({'densenet_bin_sigmoid_model': densenet_bin_sigmoid_model.predict(image_data)[0]})
-        # prediction.append({'densenet_bin_softmax_model': densenet_bin_softmax_model.predict(image_data)[0]})
+        # Select Best Model prediction
+        model1_pred = "Pneumonic" if np.argmax(pred1) == 1 else "Normal"
+        model2_pred = "Pneumonic" if np.argmax(pred2) == 1 else "Normal"
 
-        result = []
-        for index in prediction:
-            j = 0
-            for i in index:
-                # if index[i][j] == 1:
-                    result.append({'Lung Opacity': index[i][j]})
-                # else:
-                    result.append({'Normal': index[i][j+1]})
+        showMasks = False
+        print(pred1)
+        print(pred2)
 
-        return result, image_name
+        if np.argmax(pred1) > np.argmax(pred2):  # If Pred1 is Pneumonic, Pred2 is Normal
+            prediction = model1_pred
+            pred = pred1[0]
+            showMasks = True
+        elif np.argmax(pred1) == np.argmax(pred2 == 1):  # If Pred1 and Pred 2 are Pneumonic
+            showMasks = True
 
-        # prediction = model.predict(review)
-        # print("Prediction (0 = negative, 1 = positive) = ", end="")
-        # print("%0.4f" % prediction[0][0])
-        # return prediction[0][0]
+            if pred1[0][1] > pred2[0][1]:
+                prediction = model1_pred
+                pred = pred1[0]
+            else:
+                prediction = model2_pred
+                pred = pred2[0]
+        elif np.argmax(pred1) == np.argmax(pred2 == 0):  # If Pred1 and Pred 2 are Normal
+            showMasks = False
+            if pred1[0][0] > pred2[0][0]:
+                prediction = model1_pred
+                pred = pred1[0]
+            else:
+                prediction = model2_pred
+                pred = pred2[0]
+        else:  # If Pred2 is Normal and Pred2 is Pneumonic
+            showMasks = True
+            prediction = model2_pred
+            pred = pred2[0]
+
+        # print("Model Prediction:", prediction)
+        # print("     Normal    : %.2f%% \n     Pneumonic: %.2f%%" % (100 * pred[0], 100 * pred[1]))
+
+        return prediction, image_name, [100 * pred[0], 100 * pred[1]]
 
 
     def load_model_to_app(self):
